5ca9129d9d2df6d0690da08e0b6abc4a0a044521
Josef Joubert <josef.joubert@scailable.net>
2023-11-16 15:41:32 +0100

    Added python script postprocessor example

---
+++ socket-json-postprocessor-python-example/main.py
@@ -0,0 +1,77 @@
+import json
+import os
+import sys
+import socket
+
+# Add the sclbl-utilities python utilities
+script_location = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(script_location, "../sclbl-utilities/python-utilities"))
+import communication_utils
+
+# The name of the postprocessor.
+# This is used to match the definition of the postprocessor with routing.
+Postprocessor_Name = "Python-Example-Postprocessor"
+
+# The socket this postprocessor will listen on. 
+# This is always given as the first argument when the process is started
+# But it can be manually defined as well, as long as it is the same as the socket path in the runtime settings
+Postprocessor_Socket_Path = "/opt/sclbl/sockets/python-example-postprocessor.sock"
+
+def main():
+    # Validate running state
+    status = communication_utils.getScailableStatus()
+    if status["Running"] == False:
+        print("EXAMPLE PLUGIN: Postprocessor started without the Scailable Runtime running. This will probably not work.")
+    # Validate settings
+    validateSettings()
+
+    # Start socket listener to receive messages from Scailable runtime
+    server = communication_utils.startUnixSocketServer(Postprocessor_Socket_Path)
+    # Wait for messages in a loop
+    while True:
+        # Wait for input message from runtime
+        try:
+            input_message,connection = communication_utils.waitForSocketMessage(server)
+        except socket.timeout:
+            # Request timed out. Continue waiting
+            continue
+
+        print("EXAMPLE PLUGIN: Received input message: ",input_message)
+
+        # Parse input message
+        input_object = json.loads(input_message)
+        # Alter message
+        input_object["output"]["examplePostProcessor"] = "Processed"
+        output_string = json.dumps(input_object)
+
+        # Send message back to runtime
+        communication_utils.sendMessageOverConnection(connection,output_string)
+
+        # Close connection
+        connection.close()
+
+def validateSettings():
+    settings = communication_utils.getScailableSettings()
+    # Validate postprocessor is defined in 'externalPostprocessors' setting
+    external_postprocessors = []
+    if "externalPostprocessors" in settings:
+        external_postprocessors = settings["externalPostprocessors"]
+    for postprocessor in external_postprocessors:
+        if postprocessor["Name"] == Postprocessor_Name:
+            break
+    else:
+        print("EXAMPLE PLUGIN: Postprocessor started without being defined in 'externalPostprocessors' setting. This will probably not work")
+    # Validate postprocessor is assigned to a model
+    is_assigned = False
+    for model_data in settings["module"]["AssignedModels"]:
+        for assigned_postprocessor in model_data["AssignedModelPostProcessors"]:
+            if assigned_postprocessor == Postprocessor_Name:
+                is_assigned = True
+    if is_assigned == False:
+        print("EXAMPLE PLUGIN: Postprocessor started without being assigned to any model. This will probably not work")
+
+if __name__ == "__main__":
+    print("EXAMPLE PLUGIN: Input parameters: ", sys.argv)
+    if len(sys.argv) > 1:
+        Postprocessor_Socket_Path = sys.argv[1]
+    main()
