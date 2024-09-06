9eab33eb0c9f4ec85b83a2560a7acaca6648a01b
Lodewijk Evers <levers@networkoptix.com>
2024-09-06 16:14:16 +0200

    Add fingerprint to debug

--- postprocessor-python-edgeimpulse-example/postprocessor-python-edgeimpulse-example.py
+++ postprocessor-python-edgeimpulse-example/postprocessor-python-edgeimpulse-example.py
@@ -48,7 +48,7 @@ return_data = False
 # Initialize plugin and logging, script makes use of INFO and DEBUG levels
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
                     filename=LOG_FILE, filemode="w")
-logging.debug("Initializing plugin")
+logging.debug("EI PLUGIN: Initializing edge impulse plugin")
 
 def send_samples_buffer():
     # This function sends the buffered samples to an Edge Impulse instance for data processing.
@@ -60,12 +60,12 @@ def send_samples_buffer():
     # At the end, it empties the sample buffer.
     global samples_buffer, samples_counter
     if len(samples_buffer) > 0:
-        logging.info("Sending {c} samples to Edge Impulse...".format(c=len(samples_buffer)))
+        logging.info("EI PLUGIN: Sending {c} samples to Edge Impulse...".format(c=len(samples_buffer)))
         start_at = time.perf_counter()
         samples = []
         for contents in samples_buffer:
             samples_counter += 1
-            logging.info("Create sample" + str(samples_counter))
+            logging.info("EI PLUGIN: Create sample" + str(samples_counter))
             filename = "{dt}C{c}.jpg".format(dt=datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), c=samples_counter)
             output = io.BytesIO(contents)
             sample = edgeimpulse.experimental.data.Sample(
@@ -77,23 +77,23 @@ def send_samples_buffer():
             )
             samples.append(sample)
 
-        logging.info("Done creating samples, starting upload...")
+        logging.info("EI PLUGIN: Done creating samples, starting upload...")
         response = edgeimpulse.experimental.data.upload_samples(samples)
-        logging.info("Done upload")
+        logging.info("EI PLUGIN: Done upload")
 
         # Check to make sure there were no failures
         if (len(response.fails)) != 0:
-            logging.info("Could not upload files")
+            logging.info("EI PLUGIN: Could not upload files")
 
         end_at = time.perf_counter()
         logging.info("Send {c} samples in {d:0.1f}sec to Edge Impulse. Total {t}".format(
             c=len(samples_buffer), d=end_at-start_at, t=samples_counter,
         ))
-        logging.info("Send a total of {t} samples to Edge Impulse".format(t=samples_counter))
+        logging.info("EI PLUGIN: Send a total of {t} samples to Edge Impulse".format(t=samples_counter))
 
         samples_buffer = []
     else:
-        logging.info("No samples to send to Edge Impulse. Total {t}".format(t=samples_counter))
+        logging.info("EI PLUGIN: No samples to send to Edge Impulse. Total {t}".format(t=samples_counter))
 
 
 # Add the sclbl-utilities python utilities
@@ -139,7 +139,7 @@ def main():
 
         upload_sample = False
 
-        logging.debug("Wait for message " + str(counter))
+        logging.debug("EI PLUGIN: Wait for message " + str(counter))
         # Wait for input message from runtime
         try:
             input_message, connection = communication_utils.waitForSocketMessage(server)
@@ -150,7 +150,7 @@ def main():
             
         counter = counter + 1
 
-        logging.debug("Message " + str(counter) + "received")
+        logging.debug("EI PLUGIN: Message " + str(counter) + "received")
 
         # Parse input message
         parsed_response = msgpack.unpackb(input_message)
@@ -173,10 +173,10 @@ def main():
                 )
 
         if logging.getLogger().isEnabledFor(logging.DEBUG):
-            logging.debug("Message " + str(counter) + " parsed")
+            logging.debug("EI PLUGIN: Message " + str(counter) + " parsed")
             # Use pformat to format the deep object
             formatted_object = pformat(parsed_response)
-            logging.debug(f'Parsed response:\n\n{formatted_object}\n\n')
+            logging.debug(f'EI PLUGIN: Parsed response:\n\n{formatted_object}\n\n')
 
         current_time = time.time()
 
@@ -184,7 +184,7 @@ def main():
         if auto_generator and current_time - start_time >= auto_generator_every_seconds:
 
             start_time = current_time
-            logging.info("Add timed sample (every )" + str(auto_generator_every_seconds)
+            logging.info("EI PLUGIN: Add timed sample (every )" + str(auto_generator_every_seconds)
                          + "(seconds) number " + str(counter) + " to upload queue")
             upload_sample = True
 
@@ -202,7 +202,7 @@ def main():
             # Check if any of the values are below p_value and earmark result for retrieval
             for value in parsed_values:
                 if value < p_value:
-                    logging.debug("Parsed value: %.8f", value)
+                    logging.debug("EI PLUGIN: Parsed value: %.8f", value)
                     upload_sample = True
 
         if upload_sample:
@@ -238,14 +238,14 @@ def main():
 
 
 def signalHandler(sig, _):
-    logging.debug("EXAMPLE PLUGIN: Received interrupt signal: " + str(sig))
+    logging.debug("EI PLUGIN: Received interrupt signal: " + str(sig))
     if len(samples_buffer) > 0:
         send_samples_buffer()
     sys.exit(0)
 
 
 if __name__ == "__main__":
-    logging.debug("EXAMPLE PLUGIN: Input parameters: " + str(sys.argv))
+    logging.debug("EI PLUGIN: Input parameters: " + str(sys.argv))
     # Parse input arguments
     if len(sys.argv) > 1:
         Postprocessor_Socket_Path = sys.argv[1]
