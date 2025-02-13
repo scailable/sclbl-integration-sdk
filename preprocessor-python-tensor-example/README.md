d48cccfac6ec60a3f0d31d727e0682a8ee8fb545
Josef Joubert <jjoubert@networkoptix.com>
2025-02-13 16:26:10 +0100

    Updated the tensor preprocessor

--- preprocessor-python-tensor-example/README.md
+++ preprocessor-python-tensor-example/README.md
@@ -1,36 +1,51 @@
-Socket MessagePack Preprocessor Python Image Example
+Socket MessagePack Preprocessor Python Tensor Example
 =========================
 
 This example application provides an example on how to create a Python based preprocessor that can be integrated with the NXAI Edge AI Manager.
 
-This example reads an image from the AI Manager and will mirror the image horizontally. When this example is enabled, you should see the bounding boxes being mirrored with respect to the image.
+This example receives a tensor from the AI Manager and will adjust the NMS sensitivity ( if present ).
 
 # Preprocessors Control Flow
 
-The normal control flow of a preprocessor is to receive a MessagePack binary message header from the AI Manager. This message will contain information on how to connect to a Shared Memory segment which contains the input image to the AI Manager.
+The normal control flow of a preprocessor is to receive a MessagePack binary message header from the AI Manager. This message will contain information on how to connect to a Shared Memory segment which contains a MessagePack encoded tensor structure which will be given to the model.
 
-The AI Manager will send the input image to the external postprocessors before following any of its own preprocessing steps, such as resizing and normalization. This allows for the external postprocessor to operate on the original image before the rest of the pipeline. 
-For example, if an input stream to the AI Manager is YUV420, when RGB is required, a preprocessor could be added to intercept the original YUV420 image, and convert it to RGB instead.
+The AI Manager will send the tensor message after its internal preprocessing steps.
 
-The external preprocessor could write back an altered image to the shared memory segment sent by the AI Manager, or create a new shared memory segment and write a new image to the new segment. 
+The external preprocessor could write back an altered tensor to the shared memory segment sent by the AI Manager, or create a new shared memory segment and write a new tensor to the new segment. 
 
-Finally, the external postprocessor sends back a message to the AI Manager, signalling that it's completed processing. This message contains the new or same shared memory ID, image width, height and channels. These could be the same or changed. The AI Manager will read the image from the shared memory segment ID sent back to the AI Manager, and use the received image width, height and channels in the rest of the pipeline.
+Finally, the external preprocessor sends back a message to the AI Manager, signalling that it's completed processing. This message contains the new or same shared memory ID. The AI Manager will read the tensor from the shared memory segment ID sent back to the AI Manager, and pass it to the model.
 
-# MessagePack schema
+# Message schema
 
-The header message sent to the external postprocessor contains information about the image and how to retrieve it through shared memory. In Json, this schema would look like:
+Communication between the AI Manager and Tensor Preprocessor consists of two messages. A small header message is sent over socket, and a large tensor message is made available over SHM. When the header message is sent, the processor can assume that the tensor message is ready on the SHM. When the preprocessor responds with a header message over socket, the AI Manager will assume that processing is completed, and will copy back the SHM message.
+
+## Header message
+
+The header message sent to the external postprocessor is MessagePack encoded and contains information about the image and how to retrieve it through shared memory. In Json, this schema would look like:
 
 ```json
 {
     "SHMKey": <Key>,
     "SHMID": <ID>,
-    "Width": <Width>,
-    "Height": <Height>,
-    "Channels": <Channels>
 }
 ```
 
-The header message sent back to the AI Manager should contain all the same fields. The field values could be the same or changed.
+The header message sent back to the AI Manager should contain at least the SHMID field, whether the ID was changed or not.
+
+## Tensor message
+
+The tensor message can contain large amounts of binary data, and is therefore sent over shared memory as a MessagePack encoded tensor structure. In Json, this schema would look like:
+
+```json
+{
+    "Tensors": {"output_name1": [...], "output_name2": [...], ...},
+    "TensorsRanks": [rank1, rank2, ...],
+    "TensorShapes": [[shape1], [shape2], ...],
+    "TensorDataTypes": [dtype1, dtype2, ...]
+}
+```
+
+The tensor preprocessor is free to add, remove or edit tensors, as long as the tensors are compatible with the assigned model.
 
 # How to use
 
@@ -50,8 +65,8 @@ Create a configuration file at `/opt/networkoptix-metavms/mediaserver/bin/plugin
 {
     "externalPreprocessors": [
         {
-            "Name":"Example-Preprocessor",
-            "Command":"/opt/networkoptix-metavms/mediaserver/bin/plugins/nxai_plugin/nxai_manager/preprocessors/preprocessor-python-example",
+            "Name":"Example-Tensor-Preprocessor",
+            "Command":"/opt/networkoptix-metavms/mediaserver/bin/plugins/nxai_plugin/nxai_manager/preprocessors/preprocessor-python-tensor-example",
             "SocketPath":"/tmp/example-preprocessor.sock",
             "Schedule":"TENSOR"
         }
---
+++ preprocessor-python-tensor-example/preprocessor-python-tensor-example.py
@@ -0,0 +1,175 @@
+import os
+import sys
+import socket
+import signal
+import logging
+import logging.handlers
+import msgpack
+import configparser
+import struct
+
+# Add the nxai-utilities python utilities
+if getattr(sys, "frozen", False):
+    script_location = os.path.dirname(sys.executable)
+elif __file__:
+    script_location = os.path.dirname(__file__)
+sys.path.append(os.path.join(script_location, "../nxai-utilities/python-utilities"))
+import communication_utils
+
+CONFIG_FILE = os.path.join(script_location, "..", "etc", "plugin.tensor.example.ini")
+
+# Set up logging
+LOG_FILE = os.path.join(script_location, "..", "etc", "plugin.tensor.example.log")
+
+# Initialize plugin and logging, script makes use of INFO and DEBUG levels
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - example - %(message)s",
+    filename=LOG_FILE,
+    filemode="w",
+)
+
+# The name of the preprocessor.
+# This is used to match the definition of the preprocessor with routing.
+Preprocessor_Name = "Python-Tensor-Example-Preprocessor"
+
+# The socket this preprocessor will listen on.
+# This is always given as the first argument when the process is started
+# But it can be manually defined as well, as long as it is the same as the socket path in the runtime settings
+Preprocessor_Socket_Path = "/tmp/python-tensor-example-preprocessor.sock"
+
+# Define a single SHM object to share images back to AI Manager
+global output_shm
+output_shm = None
+
+
+def parseTensorFromSHM(shm_key: int):
+
+    ######### Get input tensor from SHM
+    logger.info("Got shm key: " + str(shm_key))
+    tensor_raw_data = communication_utils.read_shm(shm_key)
+    tensor_data = msgpack.unpackb(tensor_raw_data)
+
+    if (
+        tensor_data is None
+        or isinstance(tensor_data, dict) == False
+        or "Tensors" not in tensor_data
+    ):
+        logger.error("Invalid input tensor received. Ignoring.")
+        return 0
+
+    ######## Modify tensor as example
+
+    for tensor_name, _ in tensor_data["Tensors"].items():
+        logger.info("Got tensor name: " + str(tensor_name))
+        if tensor_name == "nms_sensitivity-":
+            # Set nms to 0.8
+            tensor_data["Tensors"][tensor_name] = struct.pack("f", 0.9)
+
+    ######## Write modified tensor to SHM
+
+    output_data = msgpack.packb(tensor_data)
+
+    global output_shm
+    if output_shm is None:
+        # Can reuse SHM ( if data is smaller or equal size ) or create new SHM and return ID
+        output_data_size = len(output_data)
+        output_shm = communication_utils.create_shm(output_data_size)
+        logger.debug(
+            "Created SHM with ID: "
+            + str(output_shm.id)
+            + " and size: "
+            + str(output_shm.size)
+        )
+
+    communication_utils.write_shm(output_shm, output_data)
+
+    return output_shm.id
+
+
+def main():
+    # Start socket listener to receive messages from NXAI runtime
+    server = communication_utils.startUnixSocketServer(Preprocessor_Socket_Path)
+    # Wait for messages in a loop
+    while True:
+        # Wait for input message from runtime
+        try:
+            input_message, connection = communication_utils.waitForSocketMessage(server)
+        except socket.timeout:
+            # Request timed out. Continue waiting
+            continue
+
+        image_header = msgpack.unpackb(input_message)
+        print("EXAMPLE PREPROCESSOR: Received input message: ", image_header)
+
+        # Process image
+        output_shm_id = parseTensorFromSHM(image_header["SHMKey"])
+
+        if output_shm_id != 0:
+            image_header["SHMID"] = output_shm_id
+
+        # Write header to respond
+        output_message = msgpack.packb(image_header)
+
+        # Send message back to runtime
+        communication_utils.sendMessageOverConnection(connection, output_message)
+
+
+def signalHandler(sig, _):
+    print("EXAMPLE PREPROCESSOR: Received interrupt signal: ", sig)
+    # Detach and destroy output shm
+    if output_shm is not None:
+        output_shm.detach()
+        output_shm.remove()
+    sys.exit(0)
+
+
+def config():
+    logger.info("Reading configuration from:" + CONFIG_FILE)
+
+    try:
+        configuration = configparser.ConfigParser()
+        configuration.read(CONFIG_FILE)
+
+        configured_log_level = configuration.get(
+            "common", "debug_level", fallback="INFO"
+        )
+        set_log_level(configured_log_level)
+
+        for section in configuration.sections():
+            logger.info("config section: " + section)
+            for key in configuration[section]:
+                logger.info("config key: " + key + " = " + configuration[section][key])
+
+    except Exception as e:
+        logger.error(e, exc_info=True)
+
+    logger.debug("Read configuration done")
+
+
+def set_log_level(level):
+    try:
+        logger.setLevel(level)
+    except Exception as e:
+        logger.error(e, exc_info=True)
+
+
+if __name__ == "__main__":
+    print("EXAMPLE PREPROCESSOR: Input parameters: ", sys.argv)
+    # Parse input arguments
+    if len(sys.argv) > 1:
+        Preprocessor_Socket_Path = sys.argv[1]
+    # Handle interrupt signals
+    signal.signal(signal.SIGINT, signalHandler)
+
+    ## initialize the logger
+    logger = logging.getLogger(__name__)
+
+    ## read configuration file if it's available
+    config()
+
+    logger.info("Initializing example plugin")
+    logger.debug("Input parameters: " + str(sys.argv))
+
+    # Start program
+    main()
